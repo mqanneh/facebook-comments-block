2538b7ffca5b1d07e0b037fcf626ba3079975b2a
Mohammad AlQanneh <mqanneh@gmail.com>
2015-04-14 13:49:20 +0300

    fixed coding standards + added file doc comment

--- templates/facebook-comments-theme.tpl.php
+++ templates/facebook-comments-theme.tpl.php
@@ -1,4 +1,24 @@
 <?php
+
+/**
+ * @file
+ * Default theme implementation to display a facebook comments block.
+ *
+ * Available variables:
+ * - $url: the page url.
+ * - $facebook_app_id: the facebook application id.
+ * - $facebook_app_id_script: javascript code to be appended to
+ *   the fb script to include fb app id.
+ * - $facebook_comments_block_settings_width: The width of the plugin.
+ * - $facebook_comments_block_settings_number_of_posts: The number of comments
+ *   to show by default.
+ * - $facebook_comments_block_settings_color_schema: The color scheme used by the plugin.
+ *
+ * @see template_preprocess()
+ */
+?>
+
+<?php
 print '<div id="fb-root"></div>';
 $facebook_script = '<script>(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
