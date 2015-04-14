a956ade21bdd7e29e4af5b4c930a812d0a09daab
Mohammad AlQanneh <mqanneh@gmail.com>
2015-04-14 18:53:08 +0300

    removed php from the template file to contain mainly HTML

--- templates/facebook-comments-theme.tpl.php
+++ templates/facebook-comments-theme.tpl.php
@@ -14,19 +14,15 @@
  *   to show by default.
  * - $facebook_comments_block_settings_color_schema:
  *   The color scheme used by the plugin.
- *
- * @see template_preprocess()
  */
 ?>
 
-<?php
-print '<div id="fb-root"></div>';
-$facebook_script = '<script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1' . $facebook_app_id_script . '
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, "script", "facebook-jssdk"));</script>';
-print $facebook_script;
-print '<div class="fb-comments" data-href="' . $url . '" data-width="' . $facebook_comments_block_settings_width . '" data-numposts="' . $facebook_comments_block_settings_number_of_posts . '" data-colorscheme="' . $facebook_comments_block_settings_color_schema . '"></div>';
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1<?php print $facebook_app_id_script; ?>
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, "script", "facebook-jssdk"));</script>
+<div class="fb-comments" data-href="<?php print $url; ?>" data-width="<?php print $facebook_comments_block_settings_width; ?>" data-numposts="<?php print $facebook_comments_block_settings_number_of_posts; ?>" data-colorscheme="<?php print $facebook_comments_block_settings_color_schema; ?>"></div>
