1c5b78497e7ed4b8e6b576f1e0bcc4d4b65988d0
Mohammad AlQanneh <mqanneh@gmail.com>
2015-04-15 12:34:02 +0300

    separated javascript from the template

--- templates/facebook-comments--block.tpl.php
+++ templates/facebook-comments--block.tpl.php
@@ -7,8 +7,6 @@
  * Available variables:
  * - $facebook: array contains all available variables.
  * - url: the page url.
- * - facebook_app_id: the facebook application id.
- * - facebook_app_id_script: javascript code to be appended to
  *   the fb script to include fb app id.
  * - facebook_comments_block_settings_width: The width of the plugin.
  * - facebook_comments_block_settings_number_of_posts: The number of comments
@@ -19,13 +17,4 @@
 ?>
 
 <div id="fb-root"></div>
-<script>
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1<?php print $facebook['facebook_app_id_script']; ?>";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, "script", "facebook-jssdk"));
-</script>
 <div class="fb-comments" <?php print drupal_attributes($facebook['data_attributes']); ?>></div>
