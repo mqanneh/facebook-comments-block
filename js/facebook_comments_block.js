decddbfda3f3780b04ce6f64d7b46bb73aa89898
Mohammad AlQanneh <mqanneh@gmail.com>
2015-04-15 12:41:14 +0300

    applied drupal coding standards

--- js/facebook_comments_block.js
+++ js/facebook_comments_block.js
@@ -1,8 +1,12 @@
+/*
+ * facebook_comments_block
+ *
+ */
 var facebook_app_id_script = Drupal.settings.facebook_comments_block.facebook_settings.facebook_app_id_script;
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"+facebook_app_id_script;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1" + facebook_app_id_script;
   fjs.parentNode.insertBefore(js, fjs);
 }(document, "script", "facebook-jssdk"));
