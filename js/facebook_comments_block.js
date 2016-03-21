38ca713e63762ee0b96abf40b7dc814b3411a3f8
Mohammad AlQanneh <mqanneh@gmail.com>
2016-03-21 17:02:55 +0200

    Updated facebook script.

--- js/facebook_comments_block.js
+++ js/facebook_comments_block.js
@@ -10,7 +10,7 @@ Drupal.behaviors.facebook_comments_block = {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) {return;}
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1" + facebook_app_id_script;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5" + facebook_app_id_script;
       fjs.parentNode.insertBefore(js, fjs);
     }(document, "script", "facebook-jssdk"));
   }
