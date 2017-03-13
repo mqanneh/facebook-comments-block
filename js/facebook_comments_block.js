0742492a1c7e9fdb4ca8a1ee241c86e72e44f592
mqanneh <mqanneh@2833163.no-reply.drupal.org>
2017-03-13 15:20:50 +0200

    Issue #2689269 by mqanneh: Add language settings

--- js/facebook_comments_block.js
+++ js/facebook_comments_block.js
@@ -6,11 +6,12 @@
 Drupal.behaviors.facebook_comments_block = {
   attach: function (context, settings) {
     var facebook_app_id_script = Drupal.settings.facebook_comments_block.facebook_settings.facebook_app_id_script;
+    var facebook_app_lang = Drupal.settings.facebook_comments_block.facebook_settings.facebook_app_lang;
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) {return;}
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5" + facebook_app_id_script;
+      js.src = "//connect.facebook.net/" + facebook_app_lang + "/sdk.js#xfbml=1&version=v2.8" + facebook_app_id_script;
       fjs.parentNode.insertBefore(js, fjs);
     }(document, "script", "facebook-jssdk"));
   }
